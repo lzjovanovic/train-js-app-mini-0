a8df4f29d17d2c66d806f283cd5c377204d4ea81
lzjovanovic <westhudo@gmail.com>
2021-05-20 22:26:28 +0800

    add scoring system

--- Projects/rock-paper-scissors/rock-paper-scissors.js
+++ Projects/rock-paper-scissors/rock-paper-scissors.js
@@ -1,44 +1,67 @@
-// gesture duel
-
 const choices = ["paper", "scissor", "stone"];
-
-function play_round(e) {
-  const player = e.target.value;
-  const ai = choices[Math.floor(Math.random() * 3)];
-  document.querySelector(
-    ".player"
-  ).innerHTML = `<span>you choose  ${player} </span>`;
-  document.querySelector(".ai").innerHTML = `<span>AI choose ${ai} </span>`;
-  judge(player, ai);
+let playerScore = 0;
+let aiScore = 0;
+function gameplay(e) {
+  const playerChoice = e.target.value;
+  const aiChoice = choices[Math.floor(Math.random() * 3)];
+  document.querySelector(".play-player").textContent = `${playerChoice}`;
+  document.querySelector(".play-ai").textContent = `${aiChoice}`;
+  playRound(playerChoice, aiChoice);
 }
-function judge(i, j) {
+function playRound(p1, p2) {
   let result;
-  if (i == j) {
+  if (p1 == p2) {
     result = "Tie";
-  } else if (i == choices[0]) {
-    if (j == choices[1]) {
+    document.querySelector(".result").textContent = `${result}`;
+    return;
+  }
+  if (p1 == choices[0]) {
+    if (p2 == choices[1]) {
       result = "you lose";
+      aiScore++;
     } else {
       result = "you win";
-    }
-  } else if (i == choices[1]) {
-    if (j == choices[2]) {
-      result = "you lose";
-    } else {
-      result = "you win";
-    }
-  } else if (i == choices[2]) {
-    if (j == choices[0]) {
-      result = "you lose";
-    } else {
-      result = "you win";
+      playerScore++;
     }
   }
-  document.querySelector(".result").innerHTML = `<span> ${result} </span>`;
+  if (p1 == choices[1]) {
+    if (p2 == choices[2]) {
+      result = "you lose";
+      aiScore++;
+    } else {
+      result = "you win";
+      playerScore++;
+    }
+  }
+  if (p1 == choices[2]) {
+    if (p2 == choices[0]) {
+      result = "you lose";
+      aiScore++;
+    } else {
+      result = "you win";
+      playerScore++;
+    }
+  }
+  if (playerScore == 3 || aiScore == 3) {
+    document
+      .querySelectorAll(".button")
+      .forEach((btn) => (btn.disabled = true));
+    if (playerScore == 3)
+      document.querySelector(
+        ".result-match"
+      ).textContent = `GAME OVER ! U Win !`;
+    if (aiScore == 3)
+      document.querySelector(
+        ".result-match"
+      ).textContent = `GAME OVER ! AI Win !`;
+  }
+  document.querySelector(".result").textContent = `${result}`;
+  document.querySelector(".score-player").textContent = `${playerScore}`;
+  document.querySelector(".score-ai").textContent = `${aiScore}`;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelectorAll(".button")
-    .forEach((btn) => btn.addEventListener("click", play_round));
+    .forEach((btn) => btn.addEventListener("click", gameplay));
 });
